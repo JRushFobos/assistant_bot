e55b519d0bea4432d828c334b7d6c426ecd025cc
JRushFobos <fobos_media@mail.ru>
2023-03-26 20:34:36 +0500

    project homework_bot ready for review #1

--- homework.py
+++ homework.py
@@ -1,6 +1,6 @@
 # 1. Исправить ошибки
-# 2. Доделать логирование и эксепшены
-# 3. Добавить аннотацию типов
+# 2. Доделать логирование и эксепшены +
+# 3. Добавить аннотацию типов +
 
 import os
 import logging
@@ -38,14 +38,15 @@ HOMEWORK_VERDICTS = {
 
 logging.basicConfig(
     level=logging.DEBUG,
-    filename='logs.log',
+    # filename='logs.log',
+    stream=sys.stdout,
     encoding="UTF-8",
     filemode='a',
     format='%(asctime)s, %(levelname)s, %(message)s, %(name)s',
 )
 
 
-def check_tokens():
+def check_tokens() -> bool:
     """Проверяем наличия данных в переменных в окружении."""
     if not PRACTICUM_TOKEN or not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
         logging.critical('Отсутствуют переменные или токены')
@@ -54,45 +55,35 @@ def check_tokens():
         )
 
 
-def get_api_answer(timestamp):
+def get_api_answer(timestamp: int) -> str:
     """Получаем данные из API сервиса Яндекс.Домашка."""
     try:
         response = requests.get(
             ENDPOINT, headers=HEADERS, params={'from_date': timestamp}
         )
     except Exception as error:
-        logging.error('Эндпоинт не доступен')
         raise NetworkException(f'Эндпоинт не доступен. Ошибка {error}')
     if response.status_code != HTTPStatus.OK:
-        logging.error('Статус код НЕ 200 ОК')
         raise requests.HTTPError('Статус код НЕ 200 ОК')
     try:
-        response = requests.get(
-            ENDPOINT, headers=HEADERS, params={'from_date': timestamp}
-        ).json()
+        response = response.json()
     except Exception:
-        logging.error('Не корректный формат JSON')
         raise JSONformatExceprion('Не корректный формат JSON')
     return response
 
 
-def check_response(response):
+def check_response(response: dict) -> bool:
     """Проверка наличия данных в JSON."""
     if not isinstance(response, dict):
-        logging.error('Ответ не содержит словарь')
         raise TypeError('Ответ не содержит словарь')
     elif not isinstance(response.get('homeworks'), list):
-        logging.error('Ответ не содержит списка homeworks')
         raise TypeError('Ответ не содержит списка homeworks')
     elif not response.get('current_date'):
-        logging.error('Отсутствует дата')
         raise Exception('Отсутствует дата')
 
 
-def parse_status(homework):
+def parse_status(homework: dict) -> str:
     """Парсим название и статус проекта из JSON."""
-    if 'lesson_name' not in homework:
-        raise KeyError('Ключ lesson_name отсутствует в словаре')
     if 'status' not in homework:
         raise KeyError('Ключ status отсутствует в словаре')
     if 'homework_name' not in homework:
@@ -112,10 +103,7 @@ def send_message(bot, message):
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logging.debug('Сообщение о статусе домашней работы отправлено')
     except Exception:
-        logging.error('Сообщение о статусе домашней работы НЕ отвлено')
-        raise NotSendMessageException(
-            'Отправка сообщеней в телеграмм бот недоступна'
-        )
+        logging.error('Отправка сообщеней в телеграмм бот недоступна')
 
 
 def main():
@@ -128,40 +116,21 @@ def main():
         try:
             response = get_api_answer(timestamp)
             check_response(response)
-            # Проверяем наличие ревью
-            if not response['homeworks']:
-                # send_message(bot, 'нет сообщений')
-                time.sleep(RETRY_PERIOD)
-                continue
-            new_message = parse_status(response['homeworks'][0])
-            # Проверяем наличие нового результата ревью
-            if new_message != message_with_verdict:
-                message_with_verdict = new_message
-                send_message(bot, message_with_verdict)
+            print(response)
+            if response['homeworks']:
+                new_message = parse_status(response['homeworks'][0])
+                if new_message != message_with_verdict:
+                    message_with_verdict = new_message
+                    send_message(bot, message_with_verdict)
+            else:
+                logging.debug('Новых сообщений нет')
 
         except Exception as error:
-            # message = f'Сбой в работе программы: {error}'
+            message = f'Сбой в работе программы: {error}'
+            logging.error(message)
             print(f'Сбой в работе программы: {error}')
-        time.sleep(RETRY_PERIOD)
-
-    # """Основная логика работы бота."""
-    # check_tokens()
-    # bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    # timestamp = 1549962000  # int(time.time())
-    # message_with_verdict = ''
-    # while True:
-    #     try:
-    #         response = get_api_answer(timestamp)
-    #         check_response(response)
-    #         # Проверяем наличие ревью
-    #         new_message = parse_status(response['homeworks'][0])
-    #         print(response)
-    #         send_message(bot, new_message)
-
-    #     except Exception as error:
-    #         # message = f'Сбой в работе программы: {error}'
-    #         print(f'Сбой в работе программы: {error}')
-    #     time.sleep(RETRY_PERIOD)
+        finally:
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
